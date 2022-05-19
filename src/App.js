0a87e54420692ffc0e254709b577ffd936885a8b
AmiraSoltani <amira_soltani@yahoo.fr>
2022-05-19 13:45:23 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import AddToDo from './components/AddToDo';
-import Home from './components/Home';
-import About from './components/About';
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
+
 function App() {
   return (
    
